8cc47013558d7a25f7fc42e2be4a1eb1cde9e243
parvez13913 <parvezz13913@gmail.com>
2024-01-10 21:22:48 +0600

    [DONE] Create Sidebar Options Based On User Roles

--- src/app/components/ui/Sidebar.tsx
+++ src/app/components/ui/Sidebar.tsx
@@ -10,6 +10,8 @@ import {
   TeamOutlined,
   UserOutlined,
 } from "@ant-design/icons";
+import { Sidebaritems } from "@/constants/sidebaritems";
+import { USER_ROLE } from "@/constants/role";
 
 const { Sider } = Layout;
 
@@ -46,18 +48,38 @@ const items: MenuItem[] = [
 
 const Sidebar = () => {
   const [collapsed, setCollapsed] = useState(false);
+  const role = USER_ROLE.ADMIN;
   return (
     <Sider
       collapsible
       collapsed={collapsed}
       onCollapse={(value) => setCollapsed(value)}
+      width={280}
+      style={{
+        overflow: "aut",
+        height: "100vh",
+        position: "static",
+        left: 0,
+        top: 0,
+        bottom: 0,
+      }}
     >
-      <div className="demo-logo-vertical" />
+      <div
+        style={{
+          color: "white",
+          fontSize: "2rem",
+          textAlign: "center",
+          fontWeight: "bold",
+          marginBottom: "1rem",
+        }}
+      >
+        UniTrack
+      </div>
       <Menu
         theme="dark"
         defaultSelectedKeys={["1"]}
         mode="inline"
-        items={items}
+        items={Sidebaritems(role)}
       />
     </Sider>
   );
