ae8d64338456eecda973493a505ccd4c8c28e41c
parvez13913 <parvezz13913@gmail.com>
2024-01-10 21:51:44 +0600

    [DONE] Admin Sidebar Items

--- src/constants/sidebaritems.tsx
+++ src/constants/sidebaritems.tsx
@@ -1,5 +1,9 @@
 import type { MenuProps } from "antd";
-import { UserOutlined } from "@ant-design/icons";
+import {
+  ProfileOutlined,
+  TableOutlined,
+  AppstoreOutlined,
+} from "@ant-design/icons";
 import Link from "next/link";
 
 export const Sidebaritems = (role: string) => {
@@ -8,18 +12,106 @@ export const Sidebaritems = (role: string) => {
     {
       label: "Profile",
       key: "profile",
-      icon: <UserOutlined />,
+      icon: <ProfileOutlined />,
       children: [
-        { label: "Account Profile", key: "profile" },
-        { label: "Change Password", key: "change-password" },
+        {
+          label: <Link href={`/${role}/profile`}>Account Profile</Link>,
+          key: `/${role}/profile`,
+        },
+        {
+          label: <Link href={`/${role}/change-password`}>Change Password</Link>,
+          key: `/${role}/change-password`,
+        },
       ],
     },
   ];
 
+  // Common Admin sidebar item
   const commonAdminSidebarItems: MenuProps["items"] = [
     {
       label: <Link href={`/${role}/manage-student`}>Manage Students</Link>,
-      key: "manage-students",
+      icon: <TableOutlined />,
+      key: `/${role}/manage-student`,
+    },
+    {
+      label: <Link href={`/${role}/manage-faculty`}>Manage Faculty</Link>,
+      icon: <TableOutlined />,
+      key: `/${role}/manage-faculty`,
+    },
+  ];
+
+  const adminSidebarItems: MenuProps["items"] = [
+    ...defaultSidebarItems,
+    ...commonAdminSidebarItems,
+    {
+      label: "Manage Academic",
+      key: "manage-academic",
+      icon: <TableOutlined />,
+      children: [
+        {
+          label: <Link href={`/${role}/academic/faculty`}>Faculties</Link>,
+          key: `/${role}/academic/faculty`,
+        },
+        {
+          label: <Link href={`/${role}/academic/department`}>Departments</Link>,
+          key: `/${role}/academic/department`,
+        },
+        {
+          label: <Link href={`/${role}/academic/semester`}>Semesters</Link>,
+          key: `/${role}/academic/semester`,
+        },
+      ],
+    },
+    {
+      label: "Management",
+      key: "management",
+      icon: <AppstoreOutlined />,
+      children: [
+        {
+          label: <Link href={`/${role}/department`}>Department</Link>,
+          key: `/${role}/department`,
+        },
+        {
+          label: <Link href={`/${role}/building`}>Building</Link>,
+          key: `/${role}/building`,
+        },
+        {
+          label: <Link href={`/${role}/room`}>Rooms</Link>,
+          key: `/${role}/room`,
+        },
+        {
+          label: <Link href={`/${role}/course`}>Course</Link>,
+          key: `/${role}/course`,
+        },
+        {
+          label: (
+            <Link href={`/${role}/semester-registration`}>
+              Semester Registration
+            </Link>
+          ),
+          key: `/${role}/semester-registration`,
+        },
+        {
+          label: <Link href={`/${role}/offered-course`}>Offered Course</Link>,
+          key: `/${role}/offered-course`,
+        },
+        {
+          label: (
+            <Link href={`/${role}/offered-course-section`}>
+              Offered Course Section
+            </Link>
+          ),
+          key: `/${role}/offered-course-section`,
+        },
+        {
+          label: (
+            <Link href={`/${role}/offered-course-schedule`}>
+              Offered Course Schedule
+            </Link>
+          ),
+          key: `/${role}/offered-course-schedule`,
+        },
+      ],
     },
   ];
 
