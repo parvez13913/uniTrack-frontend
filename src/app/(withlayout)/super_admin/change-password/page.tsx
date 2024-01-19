70ad59ebdeeb83c7673ac9b4a8c8736991c366a0
parvez13913 <parvezz13913@gmail.com>
2024-01-19 19:13:21 +0600

    [DONE] Change password page design

--- src/app/(withlayout)/super_admin/change-password/page.tsx
+++ src/app/(withlayout)/super_admin/change-password/page.tsx
@@ -1,7 +1,36 @@
+"use client";
+import Form from "@/components/Forms/Form";
+import FormInput from "@/components/Forms/FormInput";
+import { Button } from "antd";
+
 const ChangePasswordPage = () => {
+  const onSubmit = async (data: any) => {
+    try {
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
-    <div>
-      <h1>Change Password</h1>
+    <div
+      style={{
+        margin: "100px 0",
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <Form submitHandler={onSubmit}>
+        <h3 style={{ marginBottom: "10px" }}>Change Password</h3>
+        <div style={{ margin: "5px 0" }}>
+          <FormInput name="oldPassword" label="Old password" type="password" />
+        </div>
+        <div style={{ margin: "5px 0" }}>
+          <FormInput name="newPassword" label="New password" type="password" />
+        </div>
+        <Button type="primary" htmlType="submit" block>
+          submit
+        </Button>
+      </Form>
     </div>
   );
 };
