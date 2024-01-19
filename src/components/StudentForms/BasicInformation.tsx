1d8aeee3fb109c5d5bab618ba1f5d559b5a7ee9c
parvez13913 <parvezz13913@gmail.com>
2024-01-19 16:50:06 +0600

    [DONE] Student basic information page design

--- src/components/StudentForms/BasicInformation.tsx
+++ src/components/StudentForms/BasicInformation.tsx
@@ -1,7 +1,119 @@
+import { Col, Row } from "antd";
+import FormInput from "../Forms/FormInput";
+import FormSelectField from "../Forms/FormSelectField";
+import FormDatePicker from "../Forms/FormDatePicker";
+import { bloodGroupOptions } from "@/constants/global";
+import FormTextArea from "../Forms/FormTextArea";
+
 const BasicInformation = () => {
   return (
-    <div>
-      <h1>Basic Information</h1>
+    <div
+      style={{
+        border: "1px solid #d9d9d9",
+        borderRadius: "5px",
+        padding: "15px",
+        marginBottom: "10px",
+        marginTop: "10px",
+      }}
+    >
+      <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+        <Col
+          className="gutter-row"
+          span={8}
+          style={{
+            marginBottom: "10px",
+          }}
+        >
+          <FormInput
+            type="email"
+            name="student.email"
+            size="large"
+            label="Email"
+          />
+        </Col>
+        <Col
+          className="gutter-row"
+          span={8}
+          style={{
+            marginBottom: "10px",
+          }}
+        >
+          <FormInput
+            type="text"
+            name="student.contactNo"
+            size="large"
+            label="Contact Number"
+          />
+        </Col>
+        <Col
+          className="gutter-row"
+          span={8}
+          style={{
+            marginBottom: "10px",
+          }}
+        >
+          <FormInput
+            type="text"
+            name="student.emergencyContactNo"
+            size="large"
+            label="Emergency Contact Number"
+          />
+        </Col>
+        <Col
+          className="gutter-row"
+          span={12}
+          style={{
+            marginBottom: "10px",
+          }}
+        >
+          <FormDatePicker
+            name="student.dateOfBirth"
+            size="large"
+            label="Date Of Birth"
+          />
+        </Col>
+        <Col
+          className="gutter-row"
+          span={12}
+          style={{
+            marginBottom: "10px",
+          }}
+        >
+          <FormSelectField
+            name="student.bloodGroup"
+            size="large"
+            label="Blood Group"
+            options={bloodGroupOptions}
+            placeholder="Select"
+          />
+        </Col>
+        <Col
+          className="gutter-row"
+          span={12}
+          style={{
+            marginBottom: "10px",
+          }}
+        >
+          <FormTextArea
+            name="student.presentAddress"
+            label="Present Address"
+            rows={4}
+          />
+        </Col>
+        <Col
+          className="gutter-row"
+          span={12}
+          style={{
+            marginBottom: "10px",
+          }}
+        >
+          <FormTextArea
+            name="student.permanentAddress"
+            label="Permanent Address"
+            rows={4}
+          />
+        </Col>
+      </Row>
     </div>
   );
 };
