7f22e4e7e54288add2d020abd1d322c8abc4fdfe
parvez13913 <parvezz13913@gmail.com>
2024-01-22 16:15:33 +0600

    [DONE] useDepartmentQuery

--- src/redux/api/departmentApi.ts
+++ src/redux/api/departmentApi.ts
@@ -30,6 +30,14 @@ export const departmentApi = baseApi.injectEndpoints({
       }),
       invalidatesTags: [tagTypes.department],
     }),
+    // get single data
+    department: build.query({
+      query: (id) => ({
+        url: `${MANAGEMENT_DEPARTMENT_URL}/${id}`,
+        method: "GET",
+      }),
+      providesTags: [tagTypes.department],
+    }),
     // update department
     updateDepartment: build.mutation({
       query: (data) => ({
@@ -45,5 +53,6 @@ export const departmentApi = baseApi.injectEndpoints({
 export const {
   useDepartmentsQuery,
   useAddDepartmentMutation,
+  useDepartmentQuery,
   useUpdateDepartmentMutation,
 } = departmentApi;
