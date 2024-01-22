a3d0e94b3bc85b6bbe685862e5dbc5b0a02ee06f
parvez13913 <parvezz13913@gmail.com>
2024-01-22 19:15:44 +0600

    [DONE] Show admin page design

--- src/app/(withlayout)/super_admin/admin/page.tsx
+++ src/app/(withlayout)/super_admin/admin/page.tsx
@@ -1,10 +1,85 @@
 "use client";
 import ActionBar from "@/components/ui/ActionBar";
 import UMBreadCrumb from "@/components/ui/UMBreadCrumb";
-import { Button } from "antd";
+import UMTable from "@/components/ui/UMTable";
+import { Button, Input } from "antd";
 import Link from "next/link";
+import dayjs from "dayjs";
+import { DeleteOutlined, EditOutlined } from "@ant-design/icons";
 
 const ManageAdminPage = () => {
+  const columns = [
+    {
+      title: "Id",
+      dataIndex: "id",
+    },
+    {
+      title: "Name",
+      dataIndex: "name",
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+    },
+    {
+      title: "Department",
+      dataIndex: "department",
+    },
+    {
+      title: "Designation",
+      dataIndex: "designation",
+    },
+    {
+      title: "CreatedAt",
+      dataIndex: "createdAt",
+      render: function (data: any) {
+        return data && dayjs(data).format("MMM D, YYYY hh:mm A");
+      },
+      sorter: true,
+    },
+    {
+      title: "Action",
+      render: function (data: any) {
+        return (
+          <>
+            <Link href={`/super_admin/admin/edit/${data?.id}`}>
+              <Button
+                type="primary"
+                style={{
+                  margin: "0px 5px",
+                }}
+              >
+                <EditOutlined />
+              </Button>
+            </Link>
+            <Button type="primary" danger onClick={() => console.log(data?.id)}>
+              <DeleteOutlined />
+            </Button>
+          </>
+        );
+      },
+    },
+  ];
+
+  const tableData = [
+    {
+      key: "1",
+      name: "John",
+    },
+    {
+      key: "2",
+      name: "Jim",
+    },
+  ];
+
+  const onPaginationChange = (page: number, pageSize: number) => {
+    console.log("Page:", page, "PageSize:", pageSize);
+  };
+  const onTableChange = (pagination: any, filter: any, sorter: any) => {
+    const { order, field } = sorter;
+    console.log(order, field);
+  };
+
   return (
     <div>
       <UMBreadCrumb
@@ -17,10 +92,32 @@ const ManageAdminPage = () => {
       />
 
       <ActionBar title="Admin List">
+        <Input
+          type="text"
+          size="large"
+          placeholder="Search.."
+          style={{
+            width: "20%",
+          }}
+          onChange={() => {
+            console.log();
+          }}
+        />
         <Link href="/super_admin/admin/create">
           <Button type="primary">Create</Button>
         </Link>
       </ActionBar>
+      <UMTable
+        loading={true}
+        columns={columns}
+        dataSource={tableData}
+        pageSize={10}
+        totalPages={1}
+        showSizeChanger={true}
+        onPaginationChange={onPaginationChange}
+        onTableChange={onTableChange}
+        showPagination={true}
+      />
     </div>
   );
 };
