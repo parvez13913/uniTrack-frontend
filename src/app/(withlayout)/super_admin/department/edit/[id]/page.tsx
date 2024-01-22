4050204ff7ac38600b7f1b6b3ff387301b3f5904
parvez13913 <parvezz13913@gmail.com>
2024-01-22 16:32:49 +0600

    [DONE] Management Department edit page complete

--- src/app/(withlayout)/super_admin/department/edit/[id]/page.tsx
+++ src/app/(withlayout)/super_admin/department/edit/[id]/page.tsx
@@ -3,36 +3,52 @@ import Form from "@/components/Forms/Form";
 import FormInput from "@/components/Forms/FormInput";
 import ActionBar from "@/components/ui/ActionBar";
 import UMBreadCrumb from "@/components/ui/UMBreadCrumb";
+import {
+  useDepartmentQuery,
+  useUpdateDepartmentMutation,
+} from "@/redux/api/departmentApi";
 import { Button, Col, Row, message } from "antd";
 
 type IdProps = {
-  params: string;
+  params: any;
 };
 
 const EditDepartmentPage = ({ params }: IdProps) => {
   const { id } = params;
-  const base = "/super_admin";
+  const { data, isLoading } = useDepartmentQuery(id);
+  const [updateDepartment] = useUpdateDepartmentMutation();
 
-  const onSubmit = async (data: any) => {
+  const onSubmit = async (values: { title: string }) => {
     message.loading("Department Updating...");
     try {
+      await updateDepartment({ id, body: values });
       message.success("Department updated successfully");
     } catch (error: any) {
       await message.error(error.message);
     }
   };
 
+  const defaultValues = {
+    title: data?.title || "",
+  };
+
   return (
     <div>
       <UMBreadCrumb
         items={[
-          { label: `${base}`, link: `/${base}` },
-          { label: "department", link: `/${base}/department` },
+          {
+            label: "super_admin",
+            link: "/super_admin",
+          },
+          {
+            label: "department",
+            link: "/super_admin/department",
+          },
         ]}
       />
       <ActionBar title="Update management department" />
 
-      <Form submitHandler={onSubmit}>
+      <Form submitHandler={onSubmit} defaultValues={defaultValues}>
         <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
           <Col span={8} style={{ margin: "10px 0" }}>
             <FormInput name="title" label="Title" />
