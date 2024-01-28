78ec8cab9d8e640b9e3c8e426118bef012178e75
parvez13913 <parvezz13913@gmail.com>
2024-01-28 20:42:32 +0600

    [DONE] Create student page design

--- src/app/(withlayout)/admin/manage-student/create/page.tsx
+++ src/app/(withlayout)/admin/manage-student/create/page.tsx
@@ -1,9 +1,55 @@
 "use client";
 
+import StepperForm from "@/components/StepperForm/StepperForm";
+import BasicInformation from "@/components/StudentForms/BasicInformation";
+import GuardianInformation from "@/components/StudentForms/GuardianInformation";
+import LocalGuardianInformation from "@/components/StudentForms/LocalGuardianInformation";
+import StudentInformation from "@/components/StudentForms/StudentInformation";
+import UMBreadCrumb from "@/components/ui/UMBreadCrumb";
+
 const CreateStudentPage = () => {
+  const steps = [
+    {
+      title: "Student Information",
+      content: <StudentInformation />,
+    },
+    {
+      title: "Basic Information",
+      content: <BasicInformation />,
+    },
+    {
+      title: "Guardian Information",
+      content: <GuardianInformation />,
+    },
+    {
+      title: "Local Guardian Information",
+      content: <LocalGuardianInformation />,
+    },
+  ];
+
+  const handelStudentInfoSubmit = (data: any) => {
+    try {
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const base = "admin";
+
   return (
     <div>
-      <h1>Create Student</h1>
+      <UMBreadCrumb
+        items={[
+          { label: `${base}`, link: `/${base}` },
+          { label: "manage-student", link: `/${base}/manage-student` },
+        ]}
+      />
+      <h1 style={{ margin: "10px 0px" }}>Create Student</h1>
+      <StepperForm
+        submitHandler={(value) => handelStudentInfoSubmit(value)}
+        steps={steps}
+      />
     </div>
   );
 };
