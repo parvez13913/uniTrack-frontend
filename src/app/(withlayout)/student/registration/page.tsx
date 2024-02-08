ffd041847c33a3ecc135d21a18be0939468b7472
parvez13913 <parvezz13913@gmail.com>
2024-02-08 18:30:34 +0600

    [DONE] Student Registration Page

--- src/app/(withlayout)/student/registration/page.tsx
+++ src/app/(withlayout)/student/registration/page.tsx
@@ -1,10 +1,47 @@
 "use client";
 
+import Loading from "@/app/loading";
+import {
+  useMyRegistrationQuery,
+  useStartRegistrationMutation,
+} from "@/redux/api/semesterRegistrationApi";
+import { Button } from "antd";
+import { useRouter } from "next/navigation";
+
 const StudentRegistrationPage = () => {
+  const { data, isLoading } = useMyRegistrationQuery({});
+  const [startRegistration] = useStartRegistrationMutation();
+  const router = useRouter();
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  const goToRegistrationHandler = async () => {
+    if (!data?.studentSemesterRegistration) {
+      try {
+        await startRegistration({});
+      } catch (error) {}
+    }
+    router.push("/student/pre-registration");
+  };
+
   return (
-    <div>
-      <h1>Student registration page</h1>
-    </div>
+    <>
+      <div style={{ margin: "10px 0px" }}>
+        {data?.semesterRegistration &&
+        data?.semesterRegistration?.status === "ONGOING" &&
+        !data?.semesterRegistration?.isConfirmed ? (
+          <Button type="primary" danger onClick={goToRegistrationHandler}>
+            Go to registration
+          </Button>
+        ) : (
+          <>
+            <div>You are not allowed to do your registration. Stay tuned.</div>
+          </>
+        )}
+      </div>
+    </>
   );
 };
 
