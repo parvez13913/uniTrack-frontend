d4c98699c165b885bbc67d3355d9785781bb7f05
parvez13913 <parvezz13913@gmail.com>
2024-05-29 14:42:17 +0600

    [DONE] Get And Set New Access Token With The Help Of Refresh Token

--- src/service/auth.service.ts
+++ src/service/auth.service.ts
@@ -1,4 +1,6 @@
 import { authKey } from "@/constants/storageKey";
+import { instance as axiosInstance } from "@/helpers/axios/axiosInstance";
+import { getBaseUrl } from "@/helpers/config/envConfig";
 import { decodedToken } from "@/utils/jwt";
 import { getFromLocalStorage, setToLocalStorage } from "@/utils/local-storage";
 
@@ -23,4 +25,13 @@ export const isLoggedIn = () => {
 
 export const removeUserInfo = (key: string) => {
   return localStorage.removeItem(key);
+};
+
+export const getNewAccessToken = async () => {
+  return await axiosInstance({
+    url: `${getBaseUrl()}/auth/refreshToken`,
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    withCredentials: true,
+  });
 };
